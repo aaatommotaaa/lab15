6f238dc15a7efbbb3f17b5187df2f1d5584fb535
Atom <chanidapa.atom@gmail.com>
2026-01-30 15:19:11 +0700

    lab15_1.cpp completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,24 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+	int a = 5;
+	char b = 'A';
+	char &c = b;
+	int *x = &a;
+	char *y = &b;
+	int **z = &x;
 
-	
+	cout << a << " " << b << " " << c << " " << x << " " << (int *)y << " " << (int *)z << '\n';
+	cout << &a << " " << (int *)&b << " " << (int *)&c << " " << x << " " << (int *)y << " " << (int *)z << '\n';
+	c = 'F';
+	cout << a << " " << b << " " << c << " " << x << " " << (int *)y << " " << (int *)z << '\n';
+	*y = 'W';
+	cout << a << " " << b << " " << c << " " << x << " " << (int *)y << " " << (int *)z << '\n';
+	*x = 6;
+	cout << a << " " << b << " " << c << " " << x << " " << (int *)y << " " << (int *)z << '\n';
+	**z = 7;
+	cout << a << " " << b << " " << c << " " << x << " " << (int *)y << " " << (int *)z << '\n';
 	
 	return 0;
 }
