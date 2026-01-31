a59ca1056163b46e029f81623b7496820ed3d81b
Atom <chanidapa.atom@gmail.com>
2026-01-31 18:46:56 +0700

    lab15_2.cpp completed

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -6,7 +6,21 @@ int main(){
 	int data[] = {7,8,2,1,2,4,6,9,0,1}; 
 	cout << "Before: ";
 	for(int i = 0; i<10; i++) cout << data[i] << " ";
-	sort(&data[0],&data[10]);
+	
+	int data1[5], data2[5];
+	for (int i = 0; i<5; i++) {
+		data1[i] = data[i];
+		data2[i] = data[i+5];
+	}
+	
+	sort(&data1[0],&data1[5]);
+	sort(&data2[0],&data2[5]);
+
+	for (int i = 0; i<5; i++) {
+		data[i] = data1[i];
+		data[i+5] = data2[i];
+	}
+
 	cout << "\nAfter: ";
 	for(int i = 0; i<10; i++) cout << data[i] << " ";
 	return 0;
